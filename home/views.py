1a24f07c13e7e80d1e265459f67b847b0fa015f3
Will Prendergast <prendygast@hotmail.com>
2020-02-17 23:17:32 +0000

    got the templates working

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """
+    A view that displays an Index page
+    """
+    return render(request, "index.html")
