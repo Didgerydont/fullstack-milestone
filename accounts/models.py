7d5e39385850c9dc6f6eb327a433a25d93478e1d
Will Prendergast <prendygast@hotmail.com>
2020-02-29 14:48:44 +0000

    added reciever decoraters

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -12,3 +14,13 @@ class Profile(models.Model):
     post_code = models.CharField(max_length=45)
     country = models.CharField(max_length=45)
     birth_date = models.DateField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
