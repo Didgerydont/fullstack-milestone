778231bb1d179c2b6ac42d40cc496756df50e1f5
Will Prendergast <prendygast@hotmail.com>
2020-03-25 01:04:24 +0000

    added ItemRequest function to models

--- home/models.py
+++ home/models.py
@@ -1,13 +1,23 @@
 from django.db import models
-
+from datetime import date, datetime
 
 class PastSold(models.Model):
     name = models.CharField(max_length=254, default='')
     date_sold = models.DateTimeField(auto_now_add=True, blank=True)
     description = models.TextField()
-    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
-    finish_price = models.DecimalField(max_digits=10, decimal_places=2)
+    starting_price = models.DecimalField(max_digits=15, decimal_places=2)
+    finish_price = models.DecimalField(max_digits=15, decimal_places=2)
     image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
+
+class ItemRequest(models.Model):
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    budget = models.IntegerField()
+    image = models.ImageField(upload_to='images')
+    request_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
