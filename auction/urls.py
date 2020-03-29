dc60508d9368539d067e722865d189784477995a
Will Prendergast <prendygast@hotmail.com>
2020-03-29 23:42:50 +0000

    calling it a night

--- auction/urls.py
+++ auction/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from .models import Auction, WatchList, Bid
 from . import views
-from .views import watch_list, bids
+from .views import add_to_watch_list, bids
 
 
 app_name = "auction"
 
 urlpatterns = [
-    url(r'watchlist$', views.watch_list, name="watch_list"),
-    url(r'mybids$', views.bids, name="bids"),
+    url(r'add_to_watch_list', views.add_to_watch_list, name="add_to_watch_list"),
+    url(r'mybids', views.bids, name="bids"),
 
 ]
--- auction/views.py
+++ auction/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .models import Auction, WatchList, Bid
 
 
-def watch_list(request):
-    
-    return render(request, "mywatchlist.html")
+@login_required
+def add_to_watch_list(request, auction_id):
+    """
+    Adds an auction to the users watchlist
+    """
+    user = User.objects.filter(username=request.session['username'])
+    auction = Auction.objects.filter(id=auction_id)
+    watching = WatchList.objects.filter(auction_id=auction_id)
+    if not watching:
+        watchlist_item = WatchList()
+        watchlist_item.auction_id = auction[0]
+        watchlist_item.user_id = user[0]
+        watchlist_item.save()
+    else:
+        watching.delete()
+    return render(request, "antiques.html")
 
 def bids(request):
 
