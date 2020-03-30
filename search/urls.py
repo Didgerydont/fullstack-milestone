a9a1ece58aa08ccef6bc89bdc6b4b68cc6c76578
Will Prendergast <prendygast@hotmail.com>
2020-03-30 20:26:51 +0000

    cant get search working

--- search/urls.py
+++ search/urls.py
@@ -5,7 +5,7 @@ from .views import search_antiques, search_pastitems
 app_name = 'search'
 
 urlpatterns = [
-    url('', views.search_antiques, name='search_antiques'),
-    url('', views.search_pastitems, name='search_pastitems'),
+    url('search_antiques/', views.search_antiques, name='search_antiques'),
+    url('search_pastitems/', views.search_pastitems, name='search_pastitems'),
 
 ]
--- search/views.py
+++ search/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from antiques.models import Antiques
 from home.models import PastSold
+from django.db.models import Q
+from django.contrib import messages
 
 
 def search_antiques(request):
     """
     Search through current antiques
     """
-    found_antiques = Antiques.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "search_current.html", {"found_antiques": found_antiques})
+    return render(request, "search_current.html")
 
 
 def search_pastitems(request):
     """
     Search through past sold items
     """
-    found_pastitems = PastSold.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "search_past.html", {"found_pastitems": found_pastitems})
+    if request.method == 'GET':
+        items_search = request.GET.get('q')
+
+        items_search = []
+
+        if items_search is False or items_search == "":
+            messages.error(request, 'You must enter a term to search')
+            return redirect('index')
+
+        else:
+            check_for_matches = PastSold.objects.filter(Q(description__icontains=items_search))
+            for item in check_for_matches:
+                items_search.append(item)
+            
+    context = {
+        'items_search': items_search
+    }
+    return render(request, "search_past.html", context)
