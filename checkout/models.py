9424ee8f0331c5637b1d82c65fcbe635a945817f
Will Prendergast <prendygast@hotmail.com>
2020-03-31 06:22:25 +0000

    deleted files that had become obsolete

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from antiques.models import Antiques
+from accounts.models import Profile
+from auction.models import Auction
 
-# Create your models here.
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}".format(self.auction.pk)
--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from .views import checkout
 
+
+app_name = 'checkout'
+
 urlpatterns = [
     url(r'^$', checkout, name='checkout'),  
 ]
