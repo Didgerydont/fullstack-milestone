d33e87fa2dd465770d89871cee3a0b94e623160c
Will Prendergast <prendygast@hotmail.com>
2020-04-04 18:02:41 +0000

    recommiting after change to antiques/views.py

--- antiques/views.py
+++ antiques/views.py
@@ -12,12 +12,18 @@ def all_antiques(request):
     Displays all products for sale that are currently active in the database
     """
     antiques = Antiques.objects.all()
+    auction_information = Auction.objects.get(
+        auction_id=request.antiques.auction.auction_id
+    )
     pages = pagination(request, antiques, 4)
 
     context = {
         'items': pages[0],
         'page_range': pages[1],
-        'antiques': antiques
+        'antiques': antiques,
+        'auction_information': auction_information
     }
 
     return render(request, 'antiques.html', context)
+
+
