a3429dd8420cf3ca8f57f66d312a8391de73f356
Will Prendergast <prendygast@hotmail.com>
2020-04-12 20:24:10 +0000

    trying to get my head around the checkout

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,7 @@ import stripe
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+
 @login_required()
 def checkout_auction(request, pk):
     auction = get_object_or_404(Auction, pk=pk)
@@ -49,9 +50,68 @@ def checkout_auction(request, pk):
             if customer.paid:
                 messages.error(request, "You have successfully paid")
                 auction.money_collected = True
+                auction.antiques.bought = True
                 auction.auction_expired = True
                 auction.save()
                 return redirect(reverse('auction:auction'))
+            else:
+                messages.success(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
+
+    context = {
+        'auction': auction,
+        'total': total,
+        'order_form': order_form,
+        'payment_form': payment_form,
+        'publishable': settings.STRIPE_PUBLISHABLE_KEY,
+    }
+    return render(request, "checkout.html", context)
+
+
+@login_required()
+def buy_now_checkout(request, pk):
+    auction = get_object_or_404(Auction, pk=pk)
+
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+
+            total = auction.antiques.buy_now_price
+            order_line_item = OrderLineItem(
+                order=order,
+                auction=auction,
+                quantity=1
+            )
+            order_line_item.save()
+            order_line_item.save()
+
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100),
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+
+            if customer.paid:
+                messages.success(request, "You have successfully paid")
+                auction.money_collected = True
+                auction.antiques.bought = True
+                auction.auction_expired = True
+                auction.save()
+                return redirect(reverse('products:antiques'))
             else:
                 messages.error(request, "Unable to take payment")
         else:
@@ -69,61 +129,4 @@ def checkout_auction(request, pk):
         'publishable': settings.STRIPE_PUBLISHABLE_KEY,
     }
         
-    return render(request, "checkout.html", context)
-
-
-@login_required()
-def buy_now_checkout(request, pk):
-    antiques = get_object_or_404(Antiques, pk=pk)
-
-    if request.method == "POST":
-        order_form = OrderForm(request.POST)
-        payment_form = MakePaymentForm(request.POST)
-
-        if order_form.is_valid() and payment_form.is_valid():
-            order = order_form.save(commit=False)
-            order.date = timezone.now()
-            order.save()
-
-            total = antiques.buy_now_price
-            order_line_item = OrderLineItem(
-                order=order,
-                antiques=antiques,
-                quantity=1
-            )
-            order_line_item.save()
-            order_line_item.save()
-
-            try:
-                customer = stripe.Charge.create(
-                    amount=int(total * 100),
-                    currency="EUR",
-                    description=request.user.email,
-                    card=payment_form.cleaned_data['stripe_id'],
-                )
-            except stripe.error.CardError:
-                messages.error(request, "Your card was declined!")
-
-            if customer.paid:
-                messages.error(request, "You have successfully paid")
-                antiques.bought = True
-                antiques.save()
-                return redirect(reverse('products:antiques'))
-            else:
-                messages.error(request, "Unable to take payment")
-        else:
-            print(payment_form.errors)
-            messages.error(request, "We were unable to take a payment with that card!")
-    else:
-        payment_form = MakePaymentForm()
-        order_form = OrderForm()
-
-    context = {
-        'antiques': antiques,
-        'total': total,
-        'order_form': order_form,
-        'payment_form': payment_form,
-        'publishable': settings.STRIPE_PUBLISHABLE_KEY,
-    }
-        
-    return render(request, "checkout.html", context)
+    return render(request, "buy-now-checkout.html", context)
