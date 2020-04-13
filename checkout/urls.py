9bd07bba03f9122275fc549375305ff0e25a514a
Will Prendergast <prendygast@hotmail.com>
2020-04-13 22:42:56 +0000

    new issue local variable 'total' referenced before assignment

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,6 +6,6 @@ from .views import buy_now_checkout, checkout_auction
 app_name = 'checkout'
 
 urlpatterns = [
-    url(r'^buy_now/(?P<pk>\d+)/$', views.buy_now_checkout, name='buy_now_checkout'),
-    url(r'^checkout_auction/(?P<pk>\d+)/$', views.checkout_auction, name='checkout_auction'),   
+    url(r'^buy_now/(?P<pk>\d+)/', views.buy_now_checkout, name='buy_now_checkout'),
+    url(r'^checkout_auction/(?P<pk>\d+)/', views.checkout_auction, name='checkout_auction'),   
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -19,11 +19,11 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 @login_required()
 def checkout_auction(request, pk):
     auction = get_object_or_404(Auction, pk=pk)
-
+    
     if request.method == "POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
-
+        total = []
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
@@ -76,11 +76,11 @@ def checkout_auction(request, pk):
 @login_required()
 def buy_now_checkout(request, pk):
     auction = get_object_or_404(Auction, pk=pk)
-
+    
     if request.method == "POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
-
+        total = []
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
