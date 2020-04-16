0a8c00275fed22cc070d24e0f424ac86fc104b7d
Will Prendergast <prendygast@hotmail.com>
2020-04-16 22:55:45 +0000

    done a little more styling, will test stripe next

--- static/js/dom_manager.js
+++ static/js/dom_manager.js
@@ -7,4 +7,8 @@ $(document).ready(function(){
     $('.enquire-button').click(function() {
         $(this).next(".enquiry-field").toggle(1000);
     });
+    //Hide option to hide the profile details on smaller devices
+    $('.hide-profile').click(function() {
+        $(".profile-parent").toggle(1000);
+    });
 });
