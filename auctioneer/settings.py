8900c901523cff26e7125dcbf8dff3586052b05b
Will Prendergast <prendygast@hotmail.com>
2020-05-17 22:15:20 +0000

    the project is now on aws and this seems to have brought back the css

--- auctioneer/settings.py
+++ auctioneer/settings.py
@@ -157,7 +157,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, Dec 2099 20:00:00 GMT,
+    'Expires': 'Thu, Dec 2099 20:00:00 GMT',
     'CacheControl': 'max-age=94608000'
 }
 
@@ -168,7 +168,8 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
@@ -176,6 +177,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
